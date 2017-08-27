701fb3aa212a872844a19c740d164f6bbdcb8e44
Onesuch07 <onesuch07@gmail.com>
2017-08-27 13:37:57 +0300

    fix react

--- src/calculate.js
+++ src/calculate.js
@@ -5,52 +5,25 @@ class Calculate extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            inputs: {
-                oneNumber: '',
-                twoNumber: ''
-            },
-            result: {
-                resultValue: ''
-            },
-            buttons: {
-                pluses: '+',
-                minus: '-',
-                multiplication: '*',
-                division: '/'
-            },
+            oneNumber: '',
+            twoNumber: '',
+            resultValue: '',
             logicalExpression: '%'
         };
         this.inputChange = this.inputChange.bind(this);
     }
 
     inputChange(event) {
-        event.preventDefault();
 
-        let text = '';
+        event.preventDefault();
 
         console.log(this.state.oneNumber, this.state.twoNumber);
 
-        switch(this.state.logicalExpression) {
-            case '+':
-                text = this.state.oneNumber + this.state.twoNumber;
-                break;
-            case '-':
-                text = this.state.oneNumber - this.state.twoNumber;
-                break;
-            case '*':
-                text = this.state.oneNumber * this.state.twoNumber;
-                break;
-            case '/':
-                text = this.state.oneNumber / this.state.twoNumber;
-                break;
-        }
-
         this.setState({
-            inputs: event.target.value,
-            logicalExpression: event.target.innerText,
-            resultValue: text,
             oneNumber: Number(event.target.value),
-            twoNumber: Number(event.target.value)
+            twoNumber: Number(event.target.value),
+            logicalExpression: event.target.innerText,
+            resultValue: this.state.oneNumber + this.state.logicalExpression + this.state.twoNumber
         });
     }
 
@@ -59,18 +32,18 @@ class Calculate extends Component {
             <form className="calculate-css">
                 <h2>Калькулятор на React</h2>
                 <div className="logic">
-                    <button onClick={this.inputChange}>{this.state.buttons.pluses}</button>
-                    <button onClick={this.inputChange}>{this.state.buttons.minus}</button>
-                    <button onClick={this.inputChange}>{this.state.buttons.multiplication}</button>
-                    <button onClick={this.inputChange}>{this.state.buttons.division}</button>
+                    <button onClick={this.inputChange}>+</button>
+                    <button onClick={this.inputChange}>-</button>
+                    <button onClick={this.inputChange}>/</button>
+                    <button onClick={this.inputChange}>*</button>
                 </div>
                 <div className="calculate">
-                    <input type="text" placeholder="Первое число" onChange={this.inputChange} value={this.state.inputs.oneNumber} />
+                    <input type="text" placeholder="Первое число" onChange={this.inputChange} value={this.state.oneNumber} />
                     <span id="activeButton">{this.state.logicalExpression}</span>
-                    <input type="text" placeholder="Второе число" onChange={this.inputChange} value={this.state.inputs.twoNumber} />
+                    <input type="text" placeholder="Второе число" onChange={this.inputChange} value={this.state.twoNumber} />
                 </div>
                 <div>
-                    <input type="text" placeholder="Результат" value={this.state.result.resultValue} />
+                    <input type="text" placeholder="Результат" value={this.state.resultValue} disabled />
                 </div>
             </form>
         );
